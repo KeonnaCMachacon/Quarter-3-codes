03dc71e3deb0878047f1c11fe696a38f9c141b6d
KeonnaCMachacon <keonnapauline.machacon@smc.pshs.edu.ph>
2026-01-25 21:40:42 +0800

    Update [Machacon]_Q3_FA04.py

--- [Machacon]_Q3_FA04.py
+++ [Machacon]_Q3_FA04.py
@@ -6,24 +6,19 @@ steps = [
     [6000, 5800, 5900, 6100, 6200]
 ]
 
-# total steps for each person
 totals = []
 
 for s in steps:
     totals.append(sum(s))
 
-# print total steps
 for i in range(len(names)):
     print(names[i], "total steps:", totals[i])
 
-# find highest and lowest
 highest = max(totals)
 lowest = min(totals)
 
-# person with highest steps
 index = totals.index(highest)
 print("\nHighest total steps:", names[index], "with", highest)
 
-# difference
 difference = highest - lowest
 print("Difference between highest and lowest:", difference)
